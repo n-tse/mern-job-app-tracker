3a40b9ff040bda45b3662360131d14a541fd6443
Nick Tse <nicholashtse@gmail.com>
2023-06-02 15:29:42 -0400

    create job schema

---
+++ backend/models/job.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  submissionDate: {
+    type: Date,
+    required: true,
+  },
+  applicationStatus: {
+    type: String,
+    required: true,
+  },
+  response: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  }
+});
+
+const Job = mongoose.model("Job", JobSchema);
+
+module.exports = Job;
