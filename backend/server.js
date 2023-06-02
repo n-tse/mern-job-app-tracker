6f2c2df96b43636e34470245b558cc4f0e81e169
Nick Tse <nicholashtse@gmail.com>
2023-06-02 18:12:58 -0400

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const jobRoutes = require('./routes/JobRoutes');
 
@@ -7,6 +8,11 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 connectDB();
 
+// enables CORS for all routes with default options
+// can pass an optional 'options' object to the cors middleware
+// eg: app.use(cors({ origin:'http://localhost:5173', credentials: true }))
+// allows front and backend to communicate 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 // simple test route to check if the server is running correctly
