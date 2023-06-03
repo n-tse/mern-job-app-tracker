44e7577bf81865055b3b7ba81e0816d647e6c112
Nick Tse <nicholashtse@gmail.com>
2023-06-02 20:11:07 -0400

    add success status codes for clearer indication of req status

--- backend/controllers/JobsController.js
+++ backend/controllers/JobsController.js
@@ -2,15 +2,15 @@ const Job = require("../models/Job");
 
 exports.getAllJobs = (req, res) => {
   Job.find()
-    .then((job) => res.json(job))
+    .then((jobs) => res.status(200).json(jobs))
     .catch((e) =>
-      res.status(404).json({ message: "Job not found", error: e.message })
+      res.status(404).json({ message: "Jobs not found", error: e.message })
     );
 };
 
 exports.createJob = (req, res) => {
   Job.create(req.body)
-    .then((data) => res.json({ message: "Job successfully added", data }))
+    .then((data) => res.status(201).json({ message: "Job successfully added", data }))
     .catch((e) =>
 {      console.log(req.body);
       res.status(400).json({ message: "Failed to add job", error: e.message })}
@@ -19,7 +19,7 @@ exports.createJob = (req, res) => {
 
 exports.updateJob = (req, res) => {
   Job.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    .then((data) => res.json({ message: "Job successfully updated", data }))
+    .then((data) => res.status(200).json({ message: "Job successfully updated", data }))
     .catch((e) =>
       res
         .status(400)
@@ -28,8 +28,8 @@ exports.updateJob = (req, res) => {
 };
 
 exports.deleteJob = (req, res) => {
-  Job.findByIdAndDelete(req.params.id, req.body)
-    .then((data) => res.json({ message: "Job successfully deleted", data }))
+  Job.findByIdAndDelete(req.params.id)
+    .then(() => res.status(204).send())
     .catch((e) =>
       res
         .status(404)
