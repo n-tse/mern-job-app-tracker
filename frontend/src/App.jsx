e89695af54d2a9a9d882ec8028ffeb3522771076
Nick Tse <nicholashtse@gmail.com>
2023-06-03 11:58:33 -0400

    convert table to functional component; update associated CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
+import Table from './components/Table';
+import Modal from "./components/Modal";
 
 function App() {
   const [jobsList, setJobsList] = useState([]);
+  const [showModal, setShowModal] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -18,44 +21,10 @@ function App() {
     fetchData();
   }, []);
 
-  const convertToHeader = (string) => {
-    let res = string[0].toUpperCase();
-    for (let i = 1; i < string.length; i++) {
-      if (string[i] === string[i].toUpperCase()) {
-        res += " " + string[i];
-      } else {
-        res += string[i];
-      }
-    }
-    return res;
-  }
-  
-
   return (
     <div className="app-container">
-      <table>
-        <thead>
-          <tr>
-            {console.log(jobsList)}
-            {jobsList.length > 0 && Object.keys(jobsList[0]).map((field, idx) => field[0] !== "_" && <th key={idx}>{convertToHeader(field)}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          {jobsList.map((job) => {
-            const rowId = job._id;
-            console.log({rowId})
-            return (
-              <tr key={rowId}>
-                {Object.keys(job).map((field, fieldIdx) => {
-                  return (
-                    field[0] !== "_" && <td key={fieldIdx}>{job[field]}</td>
-                  )
-                })}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      {/* <Table jobsList={jobsList} /> */}
+      {showModal && <Modal />}
     </div>
   );
 }
---
+++ frontend/src/components/Table.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import './css/Table.css';
+
+const Table = ({ jobsList }) => {
+  
+  const convertToHeader = (string) => {
+    let res = string[0].toUpperCase();
+    for (let i = 1; i < string.length; i++) {
+      if (string[i] === string[i].toUpperCase()) {
+        res += " " + string[i];
+      } else {
+        res += string[i];
+      }
+    }
+    return res;
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {jobsList.length > 0 &&
+              Object.keys(jobsList[0]).map(
+                (field, idx) =>
+                  field[0] !== "_" && (
+                    <th key={idx}>{convertToHeader(field)}</th>
+                  )
+              )}
+          </tr>
+        </thead>
+        <tbody>
+          {jobsList.map((job) => {
+            const rowId = job._id;
+            console.log({ rowId });
+            return (
+              <tr key={rowId}>
+                {Object.keys(job).map((field, fieldIdx) => {
+                  return (
+                    field[0] !== "_" && <td key={fieldIdx}>{job[field]}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
