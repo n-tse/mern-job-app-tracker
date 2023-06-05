f4250710e8729834cac045f98784771a95f0d7c0
Nick Tse <nicholashtse@gmail.com>
2023-06-05 15:06:34 -0400

    add sorting algorithm to GET method, so default is newest to oldest

--- frontend/src/utils/handleApi.js
+++ frontend/src/utils/handleApi.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 
+// sorts response data by submissionDate from newest to oldest
 export const getJobsList = async () => {
   try {
     const response = await axios.get("http://localhost:5001/jobs/");
-    return response.data;
+    const data = response.data;
+    const sortedData = data.sort((a, b) => {
+      const dateA = new Date(a.submissionDate);
+      const dateB = new Date(b.submissionDate);
+      if (dateA > dateB) {
+        return -1;
+      } else if (dateA < dateB) {
+        return 1;
+      } else {
+        return data.indexOf(b) - data.indexOf(a);
+      }
+    });
+    return sortedData;
   } catch (error) {
     console.log(error);
   }
